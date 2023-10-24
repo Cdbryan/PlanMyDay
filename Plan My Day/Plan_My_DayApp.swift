b511243edb5da22a1aea6027396ccbf5aa0c82aa
itzelvillanueva <itzelvil@usc.edu>
2023-10-24 16:53:55 -0700

    Initial Commit

---
+++ Plan My Day/Plan_My_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plan_My_DayApp.swift
+//  Plan My Day
+//
+//  Created by Itzel Villanueva on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Plan_My_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
