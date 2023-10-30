93c4b5bdb4b15ed24206469fd8e3f4d2448ff0b1
itzelvillanueva <itzelvil@usc.edu>
2023-10-29 22:24:58 -0700

    sign up alert made

--- Plan My Day/Core/Authentication/View/SignUpView.swift
+++ Plan My Day/Core/Authentication/View/SignUpView.swift
@@ -14,8 +14,8 @@ struct SignUpView: View {
     @State private var confirmPassword = ""
     @Environment(\.dismiss) var dismiss
     @EnvironmentObject var viewModel: AuthViewModel
-    @State private var isSignUpSuccessful = false // New state variable for sign-up success
-
+    @State private var isSignUpSuccessful = false
+    @State private var showAlert = false
     
     var body: some View {
         VStack{
@@ -68,54 +68,66 @@ struct SignUpView: View {
             .padding(.horizontal)
             .padding(.top, 20)
             
-            //sign in button
-            // Sign up button
-                        Button {
-                            Task {
-                                do {
-                                    try await viewModel.createUser(withEmail: email, password: password, fullname: fullName)
-                                    isSignUpSuccessful = true
-                                } catch {
-                                    print("Error: \(error)")
-                                }
-
-                                if isSignUpSuccessful {
-                                    // Navigate to the desired view upon successful sign-up
-                                    NavigationLink(destination: ProfileView().navigationBarBackButtonHidden(true)) {
-                                        EmptyView()
-                                    }
-                                }
-                            }
-                        } label: {
-                            HStack {
-                                Text("Sign Up")
-                                    .fontWeight(.semibold)
-                                Image(systemName: "arrow.right")
-                            }
-                            .foregroundColor(.white)
-                            .frame(width: UIScreen.main.bounds.width - 32, height: 48)
-                        }
-                        .background(Color(.systemBlue))
-                        .disabled(!formIsValid)
-                        .opacity(formIsValid ? 1.0 : 0.5)
-                        .cornerRadius(10)
-                        .padding(.top, 24)
-
-                        Spacer()
-
-                        Button {
-                            dismiss()
-                        } label: {
-                            HStack(spacing: 7) {
-                                Text("Already have an account?")
-                                Text("Sign In")
-                                    .fontWeight(.bold)
-                            }
-                            .font(.system(size: 14))
-                        }
+            //sign up button
+            Button {
+                Task {
+                    do {
+                        try await viewModel.createUser(withEmail: email,
+                                                       password: password,
+                                                       fullname: fullName)
+                        isSignUpSuccessful = true
+                    } catch {
+                        print("Error: \(error)")
+                    }
+                    
+                    if isSignUpSuccessful {
+                        showAlert = true
+                        //                        // Navigate to the desired view upon successful sign-up
+                        //                        NavigationLink(destination: ProfileView().navigationBarBackButtonHidden(true)) {
+                        //                            EmptyView()
+                        //                        }
                     }
                 }
+            } label: {
+                HStack {
+                    Text("Sign Up")
+                        .fontWeight(.semibold)
+                    Image(systemName: "arrow.right")
+                }
+                .foregroundColor(.white)
+                .frame(width: UIScreen.main.bounds.width - 32, height: 48)
             }
+            .background(Color(.systemBlue))
+            .disabled(!formIsValid)
+            .opacity(formIsValid ? 1.0 : 0.5)
+            .cornerRadius(10)
+            .padding(.top, 24)
+            
+            Spacer()
+            
+            Button {
+                dismiss()
+            } label: {
+                HStack(spacing: 7) {
+                    Text("Already have an account?")
+                    Text("Sign In")
+                        .fontWeight(.bold)
+                }
+                .font(.system(size: 14))
+            }
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("Sign-Up Successful"),
+                message: Text("Your account has been created."),
+                dismissButton: .default(Text("OK")) {
+                    // No need to navigate to the ProfileView here, just dismiss the view
+                    dismiss()
+                }
+            )
+        }
+    }
+}
 
 extension SignUpView:AuthenticationFormProtocol{
     var formIsValid: Bool {
